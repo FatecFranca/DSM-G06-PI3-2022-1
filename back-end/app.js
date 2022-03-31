fecc66317f85cecbcb12f7b525c0a918d63663e5
fael0702 <rafacilio00@gmail.com>
2022-03-31 20:36:01 -0300

    aula dia 31

--- back-end/app.js
+++ back-end/app.js
@@ -2,20 +2,17 @@
 require('dotenv').config()
 
 // Exibindo as variáveis de ambiente no console (teste)
-console.log(process.env)
+//console.log(process.env)
 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 const dbConnection = require('./config/database');
-// dbConnection();
+dbConnection();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,7 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/****************************** ROTAS ******************************/
+
+const glossary = require('./routes/glossary')
+app.use('/glossary', glossary)
+
+/*******************************************************************/
 
 module.exports = app;
