f395a5d6dd2e7aefafd737e5bcdd44ea685269fa
GabrielHenrique0 <gabrielcaldeira215@gmail.com>
2022-06-22 07:03:26 +0000

    (22/06) Pterações

--- front-end/src/Header/Header.jsx
+++ front-end/src/Header/Header.jsx
@@ -4,7 +4,7 @@ import { slide as Menu2 } from 'react-burger-menu'
 import IconePerfil from '../Images/icone_perfil.png'
 import LogoErgolist from '../Images/logo_ergolist.png'
 import './Header.css';
-import ThemeSwitch from 'react-theme-switch';
+import ThemeSwitch from 'react-theme-switch/src/ThemeSwitch';
 
 class Header extends React.Component {
 
@@ -30,7 +30,7 @@ class Header extends React.Component {
                         <a href="/"><img id="logo" src={LogoErgolist} alt="Logo do site Ergolist" title="Ergolist" /></a>
 
                         <ul id="parte2">
-                            <ThemeSwitch preserveRasters/>
+                            <ThemeSwitch />
                             <li><a href="/"> Sobre </a></li>
                             <li><a href="/"> Quem somos </a></li>
                             <Menu2 id="menu2" right customBurgerIcon={<img id='iconeperfil' alt='' src={IconePerfil} />}>
